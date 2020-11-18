2c964479ea20d584a5d334ebc0ed5d90e9fdb6b7
seho <seho4815@gmail.com>
2020-11-19 03:44:09 +0900

    send User information

--- server.js
+++ server.js
@@ -6,8 +6,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello sibal'});
+app.get('/api/users', (req, res) => {
+    res.send([
+        {
+            'id' : 1,
+            'userID' : 'seho100',
+            'userPassword' : 'sop8377',
+            'email' : 'seho4815@naver.com'
+        },
+        {
+            'id' : 2,
+            'userID' : 'sunho100',
+            'userPassword' : 'sop8377',
+            'email' : 'suno100@naver.com'
+        },
+        {
+            'id' : 3,
+            'userID' : 'sungho100',
+            'userPassword' : 'sop8377',
+            'email' : 'sungho100@naver.com'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
