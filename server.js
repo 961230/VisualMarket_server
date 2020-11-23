bd0389b97d835fce7cd97629e474498c9e4a12c4
seho <seho4815@gmail.com>
2020-11-23 20:58:09 +0900

    Connection test

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { connect } = require('./config/database');
 const app = express();
 const port = process.env.PORT || 5000;
+var db_config = require('./config/database');
+var conn = db_config.init();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
 app.get('/api/users', (req, res) => {
+    var sql = 'SELECT * FROM users WHERE uname = "sunho"';
+    conn.query(sql, function(err, rows, fields){
+        if(err) console.log('query is not excuted.' + err);
+        else{
+                res.send(rows);
+        }
+    })
+})
+
+app.get('/api/user', (req, res) => {
     res.send([
         {
             'id' : 1,
